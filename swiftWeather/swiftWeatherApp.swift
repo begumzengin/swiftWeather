a5302502b1c483e62b15709aacf300b74e4d6948
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-04-22 20:29:24 +0300

    Initial Commit

---
+++ swiftWeather/swiftWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftWeatherApp.swift
+//  swiftWeather
+//
+//  Created by Begüm Zengin on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
